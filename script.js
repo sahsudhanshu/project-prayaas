9a07083b368ba122e925a8b24f52d97fb8372f37
kyan mahajan <kyanmahajan676@gmail.com>
2024-12-25 15:52:43 +0530

    345

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let img= document.querySelector('.history');
-let paras = document.getElementsByTagName('p');
+let para = document.querySelector('#pone');
 
 // Using mouseover event to detect hovering
 img.addEventListener('mouseover', () => {
     // Loop through all paragraph elements
-    for (let para of paras) {
+    
         para.innerText = 'Please donate';
         para.classList.add('phover');
-    }
+    
 });
 
 // Optional: If you want to reset the text when mouse leaves the image
 img.addEventListener('mouseout', () => {
-    for (let para of paras) {
+   
         para.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque voluptates voluptatum, aperiam fugiat accusamus soluta neque ipsa velit cupiditate, distinctio voluptate iusto minus commodi voluptatibus! Quas quo in explicabo consequatur.';
         para.classList.remove('phover') // Change this as needed
-    }
+    
 });
